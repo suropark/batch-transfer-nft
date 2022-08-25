e716f63e3a4e40196b0880be91c26276edf4f1dd
suropark <xoopp5818@gmail.com>
2022-08-25 17:31:01 +0900

    add mui

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,18 @@ import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Vault } from "./pages";
-
+import Header from "./components/Header";
 function App() {
-  return;
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Vault />} />
-      {/* <Route path="dashboard" element={<Dashboard />} /> */}
-      {/* <Route path="*" element={<NotFound />} />  */}
-    </Routes>
-  </BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Vault />} />
+        {/* <Route path="dashboard" element={<Dashboard />} /> */}
+        {/* <Route path="*" element={<NotFound />} />  */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,41 @@
+import { Box, Typography } from "@mui/material";
 import React from "react";
 
 type Props = {};
 
+const Logo = () => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        pt: 3,
+        height: "56px",
+      }}
+    >
+      <img src="/KLAY.png" alt="KLAY" height={100} />
+      <Typography
+        variant="h3"
+        sx={{
+          display: {
+            xs: "none",
+            md: "block",
+          },
+        }}
+      >
+        WKLAY
+      </Typography>
+    </Box>
+  );
+};
+
 const index = (props: Props) => {
-  return <div>index</div>;
+  return (
+    <Box sx={{}}>
+      <Logo />
+    </Box>
+  );
 };
 
 export default index;
